38b93e29a7f884c04a5c793052c7e89930503480
Qing Yang <tanqyang@gmail.com>
2017-11-05 16:35:42 +0800

    Compilation

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
-from scipy import stats
-import numpy as np
 import math
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy import stats
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV, TimeSeriesSplit
 from sklearn.metrics import make_scorer
@@ -10,7 +10,6 @@ from sklearn.cross_validation import train_test_split
 
 ## importing the models that will be trialed and tested against validation dataset
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.svm import SVR
 import xgboost as xgb
 
 store = pd.read_csv('store.csv')
@@ -18,7 +17,7 @@ train = pd.read_csv('train.csv',dtype={"StateHoliday": str})
 test = pd.read_csv('test.csv',dtype={"StateHoliday": str})
 
 """
-Functions that help initialise data and include new features to the data set to be trained
+Functions that help initialise and preprocess the data
 """
 def initialise_train_data(train, store):
     ## label our dataframe with id
@@ -27,11 +26,9 @@ def initialise_train_data(train, store):
     store = store.fillna(0)
     ## combine all features together
     df = train.merge(store, on='Store')
-    ## Get labels and remove from dataframe
-    labels = df.values[:,3]
-    labels = np.array([labels], dtype=np.int32).T
-    df = df.drop(['Sales','Id'], axis=1)
-    return (df, labels)
+    df = df.sort_values(['Id'], ascending=[1])
+    df = df.drop(['Id'], axis=1)
+    return df
 
 def initialise_test_data(test, store):
     ## label our dataframe with id
@@ -43,17 +40,24 @@ def initialise_test_data(test, store):
     df = df.sort_values(['Id'], ascending=[1])
     return df
 
+"""
+Functions that include preprocess existing features to the data set to be trained
+"""
 def set_store_close_and_sales_data(df):
     df['StoreClosedNextDay'] = df['Open']
     df['StoreClosedNextDay'] = df.StoreClosedNextDay.shift(-1)
     df['StoreClosedNextDay'] = -1 * (df['StoreClosedNextDay'] - 1)
     df['StoreClosedNextDay'][df.shape[0] - 1] = 0
+    df = df.fillna(0)
     df['StoreClosedNextDay'] = df.StoreClosedNextDay.astype(int)
 
     if 'Sales' in df.columns:
         df = df[df.Sales != 0]
-        df.drop('Sales', axis=1)
     return df
+
+## check for rows which stores are closed
+def get_closed_stores_index(df):
+    return df.ix[df['Open']==0].index
 
 ## converting dates into year, month, day and additional feature week of the year
 def date_convert(df):
@@ -92,32 +96,102 @@ def store_features(df):
                 	'IsPromoMonth'] = 1
     return df
 
+"""
+Functions that engineer new features to from the train data set and map to train & test data set
+"""
+## return a mapping the avearge sales for each store ID 
+def avg_store_sales(train):
+    dic = {}
+    for store in train.Store.unique():
+        ratio = sum(train.loc[(train['Store'] == store)].Sales)/ len(train.loc[(train['Store'] == store)].Sales)
+        dic[store] = ratio
+    return dic
+
+def map_avg_store_sales(df,mapping):
+    for key,value in mapping.items():
+        df.ix[(df.Store == key),'AvgStoreSales'] = value
+    return df
+
+## return a mapping the avearge sales for each store ID for each day of the week
+def avg_store_sales_by_week(train):
+    dic = {}
+    for store in train.Store.unique():
+        ratio_list = []
+        for day in range(1,8):
+            n = len(train.loc[(train['Store'] == store) & (train['DayOfWeek'] == day)].Sales)
+            if (n!=0):
+                ratio = sum(train.loc[(train['Store'] == store) & (train['DayOfWeek'] == day)].Sales)/ n
+            else:
+                ratio = 0
+            ratio_list.append(ratio)
+        dic[store] = ratio_list
+    return dic
+
+def map_avg_store_sales_by_week(df,mapping):
+    for key,value in mapping.items():
+        for i in range(len(value)):
+            df.ix[(df.Store == key) & (df.DayOfWeek == (i+1)),'AvgStoreSalesbyWeek'] = value[i]
+    return df
+
+## return a mapping for the average customers for each store ID for each month     
+def store_month_customers(train):
+    dic = {}
+    for store in train.Store.unique():
+        ratio_list = []
+        for month in range(1,13):
+            n = len(train.loc[(train['Store'] == store) & (train['Month'] == month)].Customers)
+            if (n!=0):
+                ratio = sum(train.loc[(train['Store'] == store) & (train['Month'] == month)].Customers)/ n
+            else:
+                ratio = 0
+            ratio_list.append(ratio)
+        dic[store] = ratio_list
+    return dic
+
+def map_store_month_customers(df,mapping):
+    for key,value in mapping.items():
+        for i in range(len(value)):
+            df.ix[(df.Store == key) & (df.Month == (i+1)),'AvgMonthlyCustomer'] = value[i]
+    return df
+
+"""
+Select the finalized features that will be used for modeling
+"""
 def select_features(df, feature_type):
-	train_features = ['Store', 'Promo', 'Promo2', 'Customers', 'SchoolHoliday', 'StoreType', 'Assortment', 'StateHoliday', 'DayOfWeek', 'Month', 'Day', 'Year', 'WeekOfYear', 'IsPromoMonth', 'StoreClosedNextDay']
-	test_features = ['Id', 'Store', 'Promo', 'Promo2', 'Customers', 'SchoolHoliday', 'StoreType', 'Assortment', 'StateHoliday', 'DayOfWeek', 'Month', 'Day', 'Year', 'WeekOfYear', 'IsPromoMonth', 'StoreClosedNextDay']
-
-	if feature_type == 'train':
-		return df[train_features]
-	return df[test_features]
-
-## check for rows which stores are closed
-def get_closed_stores_index(df):
-    return df.ix[df['Open']==0].index
+    train_features = ['Day','AvgStoreSales','AvgStoreSalesbyWeek','AvgMonthlyCustomer','Promo','Promo2','Year','Assortment','StoreType','StateHoliday','SchoolHoliday','CompetitionDistance']
+    test_features = ['Id','Day','AvgStoreSales','AvgStoreSalesbyWeek','AvgMonthlyCustomer','Promo','Promo2','Year','Assortment','StoreType','StateHoliday','SchoolHoliday','CompetitionDistance']
+    if feature_type == 'train':
+        return df[train_features]
+    return df[test_features]
 
 """
 Build Features in the order defined by feature_builders array
 """
 closed_index = get_closed_stores_index(test)
 
-train, labels = initialise_train_data(train, store)
+train = initialise_train_data(train, store)
 test = initialise_test_data(test, store)
 
-feature_builders = [set_store_close_and_sales_data, date_convert, mapping_encoding, store_features]
+feature_builders = [[set_store_close_and_sales_data], [date_convert], [mapping_encoding], [store_features], \
+                    [store_month_customers,map_store_month_customers], \
+                    [avg_store_sales_by_week,map_avg_store_sales_by_week], \
+                    [avg_store_sales,map_avg_store_sales]]
 
 for i in range(len(feature_builders)):
-	train = feature_builders[i](train)
-	test = feature_builders[i](test)
+    if (len(feature_builders[i]) == 1):
+        train = feature_builders[i][0](train)
+        test = feature_builders[i][0](test)
+    else:
+        mapping = feature_builders[i][0](train)
+        train = feature_builders[i][1](train,mapping)
+        test = feature_builders[i][1](test,mapping)
 
+#train.to_csv("cached_train.csv", index=False)
+#test.to_csv("cached_test.csv", index=False)
+#train = pd.read_csv('cached_train.csv')
+#test = pd.read_csv('cached_test.csv')
+
+labels = train.Sales
 train = select_features(train, 'train')
 test = select_features(test, 'test')
 
@@ -162,11 +236,11 @@ def optimise_hyper_parameters(train, labels, models, k, scoring_fn):
     return clfs
 
 
-models = [(params_rf, RandomForestRegressor()), (params_xgb, xgb.XGBRegressor())]
-optimal_models = optimise_hyper_parameters(train, np.log1p(labels), models, 5, make_scorer(rmspe))
+#models = [(params_rf, RandomForestRegressor()), (params_xgb, xgb.XGBRegressor())]
+#optimal_models = optimise_hyper_parameters(train, np.log1p(labels), models, 5, make_scorer(rmspe))
 
-for optimal_model in optimal_models:
-    print(optimal_model.best_params_)
+#for optimal_model in optimal_models:
+#    print(optimal_model.best_params_)
 
 
 """
@@ -187,12 +261,13 @@ y_valid = np.log1p(np.array(y_valid, dtype=np.int32))
 ## XGBoost Results using optimal parameters selected above
 params = {"objective": "reg:linear",
           "booster" : "gbtree",
-          "eta": 0.2,
-          "max_depth": 10,
+          "eta": 0.3,
+          "max_depth": 8,
           "subsample": 0.8,
           "colsample_bytree": 0.7,
           "silent": 1,
           "seed": 3244,
+          "n_estimators": 1000
           }
 
 num_boost_round = 300
@@ -209,9 +284,8 @@ pred_y = xg_model.predict(xgb.DMatrix(X_valid))
 error = rmspe(np.expm1(y_valid), np.expm1(pred_y))
 print('XGB RMSPE: {:.6f}'.format(error))
 
-
 ## Random Forest Results by using optimal parameters selected above
-rf_model = RandomForestRegressor(n_estimators=10, max_depth=8, max_features=log2)
+rf_model = RandomForestRegressor(n_estimators=10, max_depth=8, max_features='log2')
 rf_model.fit(X_train,y_train)
 pred_y = rf_model.predict(X_valid)
 error = rmspe(np.expm1(y_valid), np.expm1(pred_y))
@@ -227,9 +301,10 @@ Submission Code
 """
 # Preferably we can have a final model that is not just xgb. Or using the CV code above
 
-full_matrix = xgb.DMatrix(train, np.log1p(labels))
-final_model = xgb.train(params, full_matrix, num_boost_round, evals=watchlist, \
-  early_stopping_rounds=100, feval=rmspe_xg, verbose_eval=False) 
+#full_matrix = xgb.DMatrix(train, np.log1p(labels))
+#final_model = xgb.train(params, full_matrix, num_boost_round, evals=watchlist, \
+#  early_stopping_rounds=100, feval=rmspe_xg, verbose_eval=False) 
+
 dtest = xgb.DMatrix(test.drop('Id', axis=1))
 output = final_model.predict(dtest)
 
